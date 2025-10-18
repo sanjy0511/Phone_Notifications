119c07cc9026f201c2ca2e5036b7769f125ab107
deepak701 <deepakm6309@gmail.com>
2025-10-18 16:21:26 +0530

    telegram_bot

---
+++ bot.ts
@@ -0,0 +1,38 @@
+import TelegramBot from "node-telegram-bot-api";
+import cron from "node-cron";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const token = process.env.TOKEN!;
+const ChatId = process.env.CHAT_ID!;
+const repeat = 5; // total messages
+const intervalMs = 2000; // 2 seconds
+
+const bot = new TelegramBot(token, { polling: true });
+
+function sendNotification() {
+  let count = 0;
+
+  const interval = setInterval(() => {
+    count++;
+    bot.sendMessage(
+      ChatId,
+      `Hello Sanjay!  Time to Upload Reels (${count}/${repeat})`
+    );
+
+    if (count >= repeat) {
+      clearInterval(interval);
+      console.log(" Finished sending 5 notifications.");
+    }
+  }, intervalMs);
+}
+
+// Schedule messages at 8:30 AM and 3:37 PM
+cron.schedule("30 8 * * *", sendNotification);
+cron.schedule("50 20 * * *", sendNotification);
+
+console.log(
+  "Bot is running... Notifications will be sent 5 times (every 2 sec) at 8:30 AM & 8:50 PM daily."
+);
+// sendNotification();
